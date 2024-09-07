18fbd84c5c996561f44eb15534d89c402a33a29c
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2024-09-07 14:59:02 -0400

    refresh screen and winsize

--- editor.c
+++ editor.c
@@ -3,26 +3,69 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios org_attr;
+
+struct editorConfig{
+	int cols;
+	int rows;
+	struct termios orig_attr;
+};
+
+struct editorConfig E;
+
+int getWinSize(int *cols, int *rows){
+	struct winsize ws;
+	
+	if(ioctl(1, TIOCGWINSZ, &ws) != -1){
+		*rows = ws.ws_row;
+		*cols = ws.ws_col;
+		return 0;
+	}else{
+		return -1;
+	}
+}
+
+void drawRows(){
+	int y;	
+	getWinSize(&E.cols, &E.rows);
+	
+	for(y = 0; y < E.rows; y++){
+		write(1, "~\r\n", 3);
+	}
+	
+}
+
+void refreshScreen(){
+	write(1, "\x1b[2J", 4); //clear screen
+	write(1, "\x1b[H", 3); // move cursor home (top left)
+	
+	drawRows(); // draw tildas
+	
+	write(1, "\x1b[H", 3); // move cursor home
+}
+
+
 
 void disableRawMode(){
-	if(tcsetattr(0, TCSAFLUSH, &org_attr) == -1 ){
-		perror("tcsetattr");
+	if(tcsetattr(0, TCSAFLUSH, &E.orig_attr) == -1 ){
+		refreshScreen();
+		perror("tcsetattr");		
 		exit(1);	
 	}
 }
 
 void enableRawMode(){
 	
-	if(tcgetattr(0, & org_attr) == -1){
+	if(tcgetattr(0, & E.orig_attr) == -1){
+		refreshScreen();
 		perror("tcgetattr");
 		exit(1);
 	}
 	
-	struct termios raw = org_attr;
+	struct termios raw = E.orig_attr;
 	
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	
@@ -37,6 +80,7 @@ void enableRawMode(){
   	raw.c_cc[VTIME] = 1;
 	
 	if(tcsetattr(0, TCSAFLUSH, &raw) == -1){
+		refreshScreen();
 		perror("tcsetattr raw");
 		exit(1);
 	}
@@ -47,6 +91,7 @@ char editorReadKey() {
 	char c;
 	c = '\0';
     if(read(1, &c, 1) == -1){
+   		refreshScreen();
    		perror("read");
    		exit(1);
    	}
@@ -56,19 +101,24 @@ char editorReadKey() {
 void editorProcessKeypress() {
 	char c;
 	c = editorReadKey();
+	
+	//printf("%d\n\r", c);
 		
 	switch(c){
 		case CTRL_KEY('q'):
+			refreshScreen();
 			exit(0);
 			break;
 	}
 }
+
 
 int
 main(){	
 	enableRawMode();
 	
 	 while (1) {
+	 	refreshScreen();
     	editorProcessKeypress();
   	}
 	
